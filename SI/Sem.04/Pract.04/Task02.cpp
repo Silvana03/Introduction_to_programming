62eccc3bab49ac43b8795ea27b63b9b8860e9c47
Silvana Nikolova <78743124+Silvana03@users.noreply.github.com>
2022-10-28 10:51:55 +0300

    Create Task02.cpp

---
+++ SI/Sem.04/Pract.04/Task02.cpp
@@ -0,0 +1,16 @@
+// Task02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+bool isEven(int num)
+{
+	return  num % 2 == 0;
+}
+
+int main()
+{
+
+}
+
