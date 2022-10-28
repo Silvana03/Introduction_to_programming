21b8a4a4062778c91ac4adca3e939aac7fa3eb0b
Silvana Nikolova <78743124+Silvana03@users.noreply.github.com>
2022-10-28 10:52:01 +0300

    Create Task03.cpp

---
+++ SI/Sem.04/Pract.04/Task03.cpp
@@ -0,0 +1,20 @@
+// Task03.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+bool isPositive(int num)
+{
+	return  num > 0;
+}
+
+bool isNegative(int num)
+{
+	return  num < 0;
+}
+
+int main()
+{
+
+}
