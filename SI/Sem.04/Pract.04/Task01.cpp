32261d99edb3170bef22db05e541174a2fbe3430
Silvana Nikolova <78743124+Silvana03@users.noreply.github.com>
2022-10-28 10:43:09 +0300

    Create Task01.cpp

---
+++ SI/Sem.04/Pract.04/Task01.cpp
@@ -0,0 +1,16 @@
+// Pract04Tasks.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int sumNumbers(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+
+}
+
